8f3e9f07dea02428ee4efe185e934dfe0cedcb96
Vander Borges <vanderpborges@hotmail.com>
2020-02-14 07:19:46 -0300

    Listando categorias

--- src/main/java/com/vander/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/vander/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.vander.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vander.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "Rest funcionando";
+	public List<Categoria> listarCategoria() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
