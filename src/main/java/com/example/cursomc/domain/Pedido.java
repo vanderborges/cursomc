52a9fffb3867d23a374522c7f34a68f61d830301
Vander Borges <vander.borges@i9xp.com.br>
2020-12-01 07:05:14 -0300

    Implementacao Itens do Pedido e suas associacoes

--- src/main/java/com/example/cursomc/domain/Pedido.java
+++ src/main/java/com/example/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.example.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
@@ -34,6 +37,10 @@ public class Pedido implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "endereceo_de_entrega_id")
 	private Endereco enderecoDeEntrega;
+
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<ItemPedido>();
+	
 	
 	
 	public Pedido() {
@@ -97,6 +104,14 @@ public class Pedido implements Serializable{
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 
 	@Override
@@ -123,7 +138,6 @@ public class Pedido implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-	
+	}	
 	
 }
