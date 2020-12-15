a12e9a39c8a5e947ba8dadb2cb96c40490e78644
Vander Borges <vander.borges@i9xp.com.br>
2020-12-15 07:30:25 -0300

    Inserindo nova categoria

--- src/main/java/com/example/cursomc/services/ClienteService.java
+++ src/main/java/com/example/cursomc/services/ClienteService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.cursomc.domain.Categoria;
 import com.example.cursomc.domain.Cliente;
 import com.example.cursomc.repositories.ClienteRepository;
 import com.example.cursomc.services.exceptions.ObjectNotFoundException;
